5f5b634638c9e99779ee257f68a111e9f34a0dd2
CCotuna <calin.cotuna@emanuel.ro>
2023-05-02 00:45:29 +0300

    Styled the table

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let table = [];
 
 function setup() {
-    createCanvas(570,570);
-    background('red');
+    createCanvas(500,500);
+    background('#aaaaaa');
 
     let rows = 0;
     var c1 = 'white';
@@ -20,10 +20,10 @@ function setup() {
         let cols = 0;
         while(cols < 8){
             if(cols % 2 == 0){
-                let box = new Square(cols * 60 + 50, rows * 60 + 50, 50, c1);
+                let box = new Square(cols * 50 + 50, rows * 50 + 50, 50, c1);
                 table.push(box); 
             } else {
-                let box = new Square(cols * 60 + 50, rows * 60 + 50, 50, c2);
+                let box = new Square(cols * 50 + 50, rows * 50 + 50, 50, c2);
                 table.push(box); 
             }
             cols += 1;
@@ -31,22 +31,6 @@ function setup() {
         rows += 1;
     }
 
-    // for(let i = 0; i < 8; i++){
-    //     for(let j = 0; j < 8 ;j++){
-    //         let x = j * 60 + 50;
-    //         let y = i * 60 + 50;
-            
-    //         let c = null;
-            
-    //         if(i % 2 == 0){
-    //             c = color('#000000');
-    //         } else {
-    //             c = color('#ffffff');
-    //         }
-    //         let box = new Square(x, y, 50, c);
-    //         table.push(box); 
-    //     }
-    // }
 }
 
 function draw(){
