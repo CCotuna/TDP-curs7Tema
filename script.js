984f74803c49fbca44b1f52ae296a697be41e64a
CCotuna <calin.cotuna@emanuel.ro>
2023-05-02 00:16:49 +0300

    Implemented the table logic for setting the squares

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let table = [];
 function setup() {
     createCanvas(500,500);
     background('red');
+
+    for(let i = 0; i < 8; i++){
+        for(let j = 0; j < 8 ;j++){
+            let x = j * 60 + 50;
+            let y = i * 60 + 50;
+            let box = new Square(x, y, 50, 'red');
+            table.push(box); 
+        }
+    }
 }
 
 class Square {
